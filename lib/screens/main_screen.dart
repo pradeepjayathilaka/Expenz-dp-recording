9923accc7bf0a28b21ae4fe613f29ec1e17f6b50
pradeepjayathilaka06@gmail.com <pradeepjayathilaka06@gmail.com>
2024-10-29 23:27:12 +0530

    set the bottom navigation bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,9 @@
+import 'package:expenz/constants/colors.dart';
+import 'package:expenz/screens/add_new_screen.dart';
+import 'package:expenz/screens/budget_screen.dart';
+import 'package:expenz/screens/home_screen.dart';
+import 'package:expenz/screens/profile_screen.dart';
+import 'package:expenz/screens/transactions_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -8,12 +14,69 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
+    //screen List
+    final List<Widget> pages = [
+      HomeScreen(),
+      TransactionScreen(),
+      AddNewScreen(),
+      BudgetScreen(),
+      ProfileScreen(),
+    ];
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main Screen"),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: kWhite,
+        selectedItemColor: kMainColor,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            print(_currentIndex);
+          });
+        },
+        unselectedItemColor: kGrey,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_rounded),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: kMainColor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.add,
+                color: kWhite,
+                size: 30,
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rocket),
+            label: 'Budget',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'profile',
+          ),
+        ],
       ),
+      body: pages[_currentIndex],
     );
   }
 }
